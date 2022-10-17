ffdd5dace5959fd33142b447d963c863d46e88f0
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-17 16:44:13 +0700

    Created routes file

---
+++ lib/utils/routes.dart
@@ -0,0 +1,10 @@
+import 'package:aplikasi_kem/ui/pages/landing_screen.dart';
+import 'package:aplikasi_kem/ui/pages/splash_screen.dart';
+import 'package:flutter/widgets.dart';
+
+Map<String, Widget Function(BuildContext context)> buildRoutes() {
+  return {
+    SplashScreen.route: (ctx) => const SplashScreen(),
+    LandingScreen.route: (ctx) => const LandingScreen(),
+  };
+}
