57e3dec7274a28aa670066a88cb4ac632692c973
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-17 16:47:25 +0700

    Updated main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:aplikasi_kem/ui/pages/splash_screen.dart';
+import 'package:aplikasi_kem/utils/routes.dart';
+import 'package:aplikasi_kem/utils/ui/theme_utils.dart';
+import 'package:aplikasi_kem/utils/values/strings.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,63 +11,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Inter',
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      title: appName,
+      debugShowCheckedModeBanner: false,
+      theme: getCustomThemeData(),
+      routes: buildRoutes(),
+      initialRoute: SplashScreen.route,
     );
   }
 }
