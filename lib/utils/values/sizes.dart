c41c9764868dd07667bceb7aa110bd9487a51e9d
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-17 16:38:41 +0700

    Created size values util file

---
+++ lib/utils/values/sizes.dart
@@ -0,0 +1,10 @@
+// Text Sizes
+const textSizeVerySmall = 12.0;
+const textSizeSmall = 14.0;
+const textSizeRegular = 15.0;
+const textSizeMedium = 16.0;
+const textSizeLarge = 18.0;
+const textSizeHeading = 20.0;
+const textSizeSplashScreenTagline = 32.0;
+
+// Margins and Paddings
