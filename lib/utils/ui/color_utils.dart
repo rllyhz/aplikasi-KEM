c789fff6adf87cc17822805394600057be57b4b0
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-17 17:35:05 +0700

    Added some theming colors into project

--- lib/utils/ui/color_utils.dart
+++ lib/utils/ui/color_utils.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
-const blueColor = Color(0xFF115FC4);
-const yellowColor = Color(0xFFFFC700);
 const backgroundColor = Color(0xFFF5F5F5);
 const greyTextColor = Color.fromRGBO(28, 28, 255, 0.11);
 const whiteColor = Color(0xFFFFFFFF);
 const errorColor = Colors.redAccent;
 const headingTextColor = Color(0xFF1C1C1C);
 const darkTextColor = Color(0xFF1C1C1C);
+
+const silverColor = Color(0xFF6F6F6F);
+const blueColor = Color(0xFF115FC4);
+const yellowColor = Color(0xFFFFC700);
+const redColor = Color(0xFFE94A47);
+const orangeColor = Color(0xFFEE942A);
+const limeColor = Color(0xFF00AFAF);
+const darkPurpleColor = Color(0xFF1D225F);
+const lightBlueColor = Color(0xFF709CDF);
+const greenColor = Color(0xFF55B95F);
 
 ColorScheme getCustomColorScheme() {
   return const ColorScheme.light(
