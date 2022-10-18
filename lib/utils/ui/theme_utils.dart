820c58d11e8ef2b0846744337897918c04e22b40
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-19 00:58:41 +0700

    Fixed theme | Updated theme_utils.dart file

--- lib/utils/ui/theme_utils.dart
+++ lib/utils/ui/theme_utils.dart
@@ -1,11 +1,11 @@
-import 'package:aplikasi_kem/utils/ui/color_utils.dart';
-import 'package:aplikasi_kem/utils/ui/font_utils.dart';
+import 'package:aplikasi_kem/utils/ui/color_utils.dart' as colors;
+import 'package:aplikasi_kem/utils/ui/font_utils.dart' as text_styles;
 import 'package:flutter/material.dart';
 
 ThemeData getCustomThemeData() {
   return ThemeData(
-    fontFamily: customFontFamilyName,
-    colorScheme: getCustomColorScheme(),
-    textTheme: getCustomTextTheme(),
+    fontFamily: text_styles.customFontFamilyName,
+    colorScheme: colors.getCustomColorScheme(),
+    textTheme: text_styles.getCustomTextTheme(),
   );
 }
