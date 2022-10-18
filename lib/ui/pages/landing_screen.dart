9d9185930694fc08dcdc4bea5cb8e79b1e8beb1b
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-19 01:07:50 +0700

    Fixed landing screen | Added action button to the landing screen

--- lib/ui/pages/landing_screen.dart
+++ lib/ui/pages/landing_screen.dart
@@ -1,3 +1,7 @@
+import 'package:aplikasi_kem/ui/widgets/custom_button.dart';
+import 'package:aplikasi_kem/ui/widgets/space.dart';
+import 'package:aplikasi_kem/utils/ui/color_utils.dart' as colors;
+import 'package:aplikasi_kem/utils/values/sizes.dart' as sizes;
 import 'package:flutter/material.dart';
 
 class LandingScreen extends StatelessWidget {
@@ -8,13 +12,76 @@ class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Hello there!',
-          style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                color: Colors.grey,
-              ),
+      backgroundColor: colors.whiteColor,
+      body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Stack(
+          children: <Widget>[
+            _buildIllustrationImage(
+              context,
+              'assets/images/landing/shaped_background.png',
+            ),
+            Column(
+              children: [
+                _buildIllustrationImage(
+                  context,
+                  'assets/images/landing/all_illustration.png',
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Ayo Belajar Sekarang!',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline2?.copyWith(
+                              fontSize: sizes.textSizeLarge,
+                              color: colors.whiteColor,
+                            ),
+                      ),
+                      const Space(
+                        size: 10.0,
+                      ),
+                      Text(
+                        'Petualangan dalam hidup adalah seberapa banyak kita belajar.',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                              color: colors.whiteColor,
+                              fontSize: 15.0,
+                            ),
+                      ),
+                      const Space(
+                        size: 120.0,
+                      ),
+                      CustomButton(
+                        text: 'Yuk Mulai!',
+                        textColor: colors.whiteColor,
+                        onPressed: () {},
+                      ),
+                      const Space(
+                        size: 120.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildIllustrationImage(
+    BuildContext context,
+    String assetName,
+  ) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Image.asset(
+        assetName,
+        fit: BoxFit.fill,
       ),
     );
   }
