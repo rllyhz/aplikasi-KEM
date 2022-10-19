98de1c894a543c83dc25a16c49920466526ca2e1
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-19 19:57:22 +0700

    Created colored_card_button widget

---
+++ lib/ui/widgets/colored_card_button.dart
@@ -0,0 +1,51 @@
+import 'package:aplikasi_kem/ui/widgets/colored_card.dart';
+import 'package:aplikasi_kem/ui/widgets/next_icon_button.dart';
+import 'package:aplikasi_kem/utils/ui/color_utils.dart' as colors;
+import 'package:flutter/material.dart';
+
+class ColoredCardButton extends StatelessWidget {
+  const ColoredCardButton({
+    super.key,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.label,
+    this.child,
+    this.onIconPressed,
+  });
+
+  final Color backgroundColor;
+  final Color textColor;
+  final String label;
+  final Widget? child;
+  final Function()? onIconPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredCard(
+      borderRadiusSize: 82.0,
+      padding: const EdgeInsets.all(0),
+      backgroundColor: backgroundColor,
+      child: Padding(
+        padding: const EdgeInsets.only(
+            top: 4.0, bottom: 4.0, left: 24.0, right: 4.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              label,
+              style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                    color: textColor,
+                  ),
+            ),
+            NextIconButton(
+              onIconPressed: onIconPressed,
+              backgroundColor: colors.whiteColor,
+              iconColor: backgroundColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
