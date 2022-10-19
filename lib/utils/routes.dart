2d8bcd05f4f53809e2e4eb5ebd57d3436e24243e
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-19 20:00:53 +0700

    Added reading screen into the routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,5 +1,6 @@
 import 'package:aplikasi_kem/ui/pages/dashboard_screen.dart';
 import 'package:aplikasi_kem/ui/pages/landing_screen.dart';
+import 'package:aplikasi_kem/ui/pages/reading_screen.dart';
 import 'package:aplikasi_kem/ui/pages/splash_screen.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,6 +9,7 @@ Map<String, Widget Function(BuildContext context)> build() {
     SplashScreen.route: (ctx) => const SplashScreen(),
     LandingScreen.route: (ctx) => const LandingScreen(),
     DashboardScreen.route: (ctx) => const DashboardScreen(),
+    ReadingScreen.route: (ctx) => const ReadingScreen(),
   };
 }
 
@@ -17,4 +19,10 @@ void navigateTo(BuildContext context, String routeName) {
 
 void replaceWith(BuildContext context, String routeName) {
   Navigator.pushReplacementNamed(context, routeName);
+}
+
+void navigateUp(BuildContext context) {
+  if (Navigator.canPop(context)) {
+    Navigator.pop(context);
+  }
 }
