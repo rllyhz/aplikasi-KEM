0d5704f38e5b4ddd902393ff0ab26023dceb329d
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-21 01:12:19 +0700

    Added reading_first_section screen into routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,5 +1,6 @@
 import 'package:aplikasi_kem/ui/pages/dashboard_screen.dart';
 import 'package:aplikasi_kem/ui/pages/landing_screen.dart';
+import 'package:aplikasi_kem/ui/pages/reading/reading_first_section_screen.dart';
 import 'package:aplikasi_kem/ui/pages/reading/reading_screen.dart';
 import 'package:aplikasi_kem/ui/pages/splash_screen.dart';
 import 'package:flutter/widgets.dart';
@@ -10,6 +11,7 @@ Map<String, Widget Function(BuildContext context)> build() {
     LandingScreen.route: (ctx) => const LandingScreen(),
     DashboardScreen.route: (ctx) => const DashboardScreen(),
     ReadingScreen.route: (ctx) => const ReadingScreen(),
+    ReadingFirstSectionScreen.route: (ctx) => const ReadingFirstSectionScreen(),
   };
 }
 
