8ae5a17bc2b779c68b927d94e9d70ee536065863
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-21 01:29:42 +0700

    Added reading_second_section screen to the routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,6 +2,7 @@ import 'package:aplikasi_kem/ui/pages/dashboard_screen.dart';
 import 'package:aplikasi_kem/ui/pages/landing_screen.dart';
 import 'package:aplikasi_kem/ui/pages/reading/reading_first_section_screen.dart';
 import 'package:aplikasi_kem/ui/pages/reading/reading_screen.dart';
+import 'package:aplikasi_kem/ui/pages/reading/reading_second_section_screen.dart';
 import 'package:aplikasi_kem/ui/pages/splash_screen.dart';
 import 'package:flutter/widgets.dart';
 
@@ -12,6 +13,8 @@ Map<String, Widget Function(BuildContext context)> build() {
     DashboardScreen.route: (ctx) => const DashboardScreen(),
     ReadingScreen.route: (ctx) => const ReadingScreen(),
     ReadingFirstSectionScreen.route: (ctx) => const ReadingFirstSectionScreen(),
+    ReadingSecondSectionScreen.route: (ctx) =>
+        const ReadingSecondSectionScreen(),
   };
 }
 
