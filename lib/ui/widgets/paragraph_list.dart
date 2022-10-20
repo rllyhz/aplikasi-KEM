0113fe6fe97ccc90e636c3777d9c475e3e0aa8b0
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-21 02:44:32 +0700

    Updated paragraph_list widget | Updaing numbering type and numbering suffix

---
+++ lib/ui/widgets/paragraph_list.dart
@@ -0,0 +1,54 @@
+import 'package:aplikasi_kem/ui/widgets/space.dart';
+import 'package:aplikasi_kem/utils/ui/text_numbering_helpers.dart';
+import 'package:flutter/material.dart';
+import 'package:aplikasi_kem/utils/values/sizes.dart' as sizes;
+import 'package:aplikasi_kem/utils/ui/color_utils.dart' as colors;
+
+class ParagraphList extends StatelessWidget {
+  const ParagraphList({
+    super.key,
+    required this.list,
+    this.numberingType = NumberingType.numeric,
+    this.numberingSuffix = NumberingSuffix.dot,
+  });
+
+  final List<String> list;
+  final NumberingType numberingType;
+  final NumberingSuffix numberingSuffix;
+
+  @override
+  Widget build(BuildContext context) {
+    var textStyle = Theme.of(context).textTheme.bodyText2?.copyWith(
+          color: colors.darkTextColor,
+          fontSize: sizes.textSizeMedium,
+          height: sizes.textLineHeightParagraph,
+        );
+
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: list.length,
+      itemBuilder: (ctx, index) => Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            _getFormattedNumberingText(index),
+            style: textStyle,
+          ),
+          const Space(size: 8.0, orientation: SpaceOrientation.vertical),
+          Expanded(
+            child: Text(
+              list[index],
+              style: textStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _getFormattedNumberingText(int index) =>
+      getNumberingByIndex(index, numberingType) +
+      getNumberingSuffixOf(numberingSuffix);
+}
