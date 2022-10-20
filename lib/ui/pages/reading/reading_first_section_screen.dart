5a40bc7f51161a1faa08d311575538d0462e9ca1
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-21 01:11:42 +0700

    Created reading_first_section screen

---
+++ lib/ui/pages/reading/reading_first_section_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ReadingFirstSectionScreen extends StatelessWidget {
+  const ReadingFirstSectionScreen({super.key});
+
+  static const String route = '/reading-first-section';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
