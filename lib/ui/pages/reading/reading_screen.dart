9c4cf1879ebe6e831b6a0a7948146b0faffe42f3
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-21 03:55:47 +0700

    Updated reading screen UI | Implementing onIconPressed of all the rest of menu buttons

--- lib/ui/pages/reading/reading_screen.dart
+++ lib/ui/pages/reading/reading_screen.dart
@@ -1,5 +1,8 @@
+import 'package:aplikasi_kem/ui/pages/reading/reading_fifth_section_screen.dart';
 import 'package:aplikasi_kem/ui/pages/reading/reading_first_section_screen.dart';
+import 'package:aplikasi_kem/ui/pages/reading/reading_fourth_section_screen.dart';
 import 'package:aplikasi_kem/ui/pages/reading/reading_second_section_screen.dart';
+import 'package:aplikasi_kem/ui/pages/reading/reading_third_section_screen.dart';
 import 'package:aplikasi_kem/ui/widgets/colored_card_button.dart';
 import 'package:aplikasi_kem/ui/widgets/main_detail_scaffold.dart';
 import 'package:aplikasi_kem/ui/widgets/space.dart';
@@ -44,21 +47,27 @@ class ReadingScreen extends StatelessWidget {
             label: 'Manfaat Membaca',
             textColor: colors.whiteColor,
             backgroundColor: colors.limeColor,
-            onIconPressed: () {},
+            onIconPressed: () {
+              routes.navigateTo(context, ReadingThirdSectionScreen.route);
+            },
           ),
           const Space(size: 8.0),
           ColoredCardButton(
             label: 'Aspek Membaca',
             textColor: colors.whiteColor,
             backgroundColor: colors.darkPurpleColor,
-            onIconPressed: () {},
+            onIconPressed: () {
+              routes.navigateTo(context, ReadingFourthSectionScreen.route);
+            },
           ),
           const Space(size: 8.0),
           ColoredCardButton(
             label: 'Ringkasan',
             textColor: colors.whiteColor,
             backgroundColor: colors.lightBlueColor,
-            onIconPressed: () {},
+            onIconPressed: () {
+              routes.navigateTo(context, ReadingFifthSectionScreen.route);
+            },
           ),
         ],
       ),
