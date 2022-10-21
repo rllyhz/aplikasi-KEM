361f9b826b4cf59c09df954fdd0796d74f3c20f8
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-22 01:33:50 +0700

    Added functionality to set statusbar color in runtime

--- lib/utils/ui/theme_utils.dart
+++ lib/utils/ui/theme_utils.dart
@@ -1,11 +1,22 @@
 import 'package:aplikasi_kem/utils/ui/color_utils.dart' as colors;
 import 'package:aplikasi_kem/utils/ui/font_utils.dart' as text_styles;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 ThemeData getCustomThemeData() {
   return ThemeData(
+    visualDensity: VisualDensity.adaptivePlatformDensity,
     fontFamily: text_styles.customFontFamilyName,
     colorScheme: colors.getCustomColorScheme(),
     textTheme: text_styles.getCustomTextTheme(),
+  );
+}
+
+void setStatusBarColor(Color color) {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.light,
+      statusBarColor: color,
+    ),
   );
 }
