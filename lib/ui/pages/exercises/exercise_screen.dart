3bab08c153c4f45356e07a9e3eae6a3a79e30841
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-24 23:32:26 +0700

    Updated exercise screen

--- lib/ui/pages/exercises/exercise_screen.dart
+++ lib/ui/pages/exercises/exercise_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:aplikasi_kem/utils/ui/color_utils.dart' as colors;
 
 class ExerciseScreen extends StatelessWidget {
   const ExerciseScreen({super.key});
@@ -7,6 +8,14 @@ class ExerciseScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: colors.backgroundColor,
+      body: Center(
+        child: Text(
+          'Exercise screen',
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ),
+    );
   }
 }
