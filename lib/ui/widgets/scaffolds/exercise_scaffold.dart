3430ba52552d3cfaa6efa7a2ca1248013cf8457d
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-24 16:59:07 +0700

    Created general_scaffold and exercise_scaffold widgets

---
+++ lib/ui/widgets/scaffolds/exercise_scaffold.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExerciseScaffold extends StatelessWidget {
+  const ExerciseScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
