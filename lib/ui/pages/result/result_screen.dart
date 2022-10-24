a2bb0172343cc36c9a2426deee89bc1c9717fd00
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-24 23:32:41 +0700

    Updated result screen

--- lib/ui/pages/result/result_screen.dart
+++ lib/ui/pages/result/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:aplikasi_kem/utils/ui/color_utils.dart' as colors;
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
@@ -7,6 +8,14 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: colors.backgroundColor,
+      body: Center(
+        child: Text(
+          'Result screen',
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ),
+    );
   }
 }
