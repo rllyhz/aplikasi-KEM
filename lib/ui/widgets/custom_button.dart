8b60a2cf3e4f09df4c6fe623b580ad30c1a20775
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-25 23:26:02 +0700

    Fixed up custom_button widget

--- lib/ui/widgets/custom_button.dart
+++ lib/ui/widgets/custom_button.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:aplikasi_kem/utils/values/sizes.dart' as sizes;
+
+enum CustomButtonType {
+  small,
+  medium,
+  large,
+}
 
 class CustomButton extends StatelessWidget {
   const CustomButton({
@@ -10,6 +17,10 @@ class CustomButton extends StatelessWidget {
     this.borderRadiusSize,
     this.padding,
     this.fontSize,
+    this.type = CustomButtonType.medium,
+    this.textAlign = TextAlign.start,
+    this.fontStyle = FontStyle.normal,
+    this.decoration = TextDecoration.none,
   });
 
   final String text;
@@ -18,6 +29,10 @@ class CustomButton extends StatelessWidget {
   final double? fontSize;
   final double? borderRadiusSize;
   final EdgeInsetsGeometry? padding;
+  final CustomButtonType type;
+  final TextAlign textAlign;
+  final FontStyle fontStyle;
+  final TextDecoration decoration;
   final Function() onPressed;
 
   @override
@@ -36,19 +51,67 @@ class CustomButton extends StatelessWidget {
         elevation: MaterialStateProperty.all<double>(4.0),
       ),
       child: Padding(
-        padding: padding ??
-            const EdgeInsets.symmetric(
-              vertical: 12.0,
-              horizontal: 36.0,
-            ),
+        padding: padding ?? _getPadding(),
         child: Text(
           text,
-          style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                color: textColor,
-                fontSize: fontSize ?? 18.0,
-              ),
+          style: _getStyle(context),
+          textAlign: textAlign,
         ),
       ),
     );
+  }
+
+  TextStyle? _getStyle(BuildContext context) {
+    if (type == CustomButtonType.small) {
+      return Theme.of(context).textTheme.bodyText2?.copyWith(
+            color: textColor,
+            fontSize: fontSize ?? _getFontSize(),
+            fontStyle: fontStyle,
+            decoration: decoration,
+          );
+    } else if (type == CustomButtonType.medium) {
+      return Theme.of(context).textTheme.bodyText1?.copyWith(
+            color: textColor,
+            fontSize: fontSize ?? _getFontSize(),
+            fontStyle: fontStyle,
+            decoration: decoration,
+          );
+    } else {
+      return Theme.of(context).textTheme.bodyText1?.copyWith(
+            color: textColor,
+            fontSize: fontSize ?? _getFontSize(),
+            fontStyle: fontStyle,
+            decoration: decoration,
+          );
+    }
+  }
+
+  double _getFontSize() {
+    if (type == CustomButtonType.small) {
+      return sizes.textSizeSmall;
+    } else if (type == CustomButtonType.medium) {
+      return sizes.textSizeMedium;
+    } else {
+      return 22.0;
+    }
+  }
+
+  EdgeInsetsGeometry _getPadding() {
+    if (type == CustomButtonType.small) {
+      return const EdgeInsets.symmetric(
+        vertical: 8.0,
+        horizontal: 18.0,
+      );
+    } else if (type == CustomButtonType.medium) {
+      return const EdgeInsets.symmetric(
+        vertical: 12.0,
+        horizontal: 36.0,
+      );
+    } else {
+      return const EdgeInsets.symmetric(
+        vertical: 14.0,
+        horizontal: 56.0,
+      );
+    }
   }
 }
