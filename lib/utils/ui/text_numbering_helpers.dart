cd0b2f100bcf9fa210fe4dcfc1770863e7769af9
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-26 01:29:39 +0700

    Added timer helper to format counter in seconds into formatted string '00:00:00'

--- lib/utils/ui/text_numbering_helpers.dart
+++ lib/utils/ui/text_numbering_helpers.dart
@@ -41,3 +41,32 @@ String getNumberingSuffixOf(NumberingSuffix numberingSuffix) {
     return "";
   }
 }
+
+String getTimerStringOf(int countInSeconds) {
+  // counts / (60 * 60) => hours
+  // counts / 60 => mins
+  // counts & 60 => seconds
+
+  // Ex: 120 counts (seconds)
+  // 120 / (60 * 60) = 0.03333333333333333
+  // 120 / 60 = 2
+  // 120 mod 60 = 0
+
+  var hours = countInSeconds ~/ (60 * 60);
+  var minutes = countInSeconds ~/ 60;
+  var seconds = countInSeconds % 60;
+
+  var formattedHours = _formatTime(hours);
+  var formattedMinutes = _formatTime(minutes);
+  var formattedSeconds = _formatTime(seconds);
+
+  return '$formattedHours:$formattedMinutes:$formattedSeconds';
+}
+
+String _formatTime(int time) {
+  if (time < 10) {
+    return "0$time";
+  } else {
+    return time.toString();
+  }
+}
