39adc98f2852e4c59e5f3e1a9d1942aaf9878c56
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-26 18:56:45 +0700

    Created result screen

--- lib/ui/pages/result/result_screen.dart
+++ lib/ui/pages/result/result_screen.dart
@@ -1,3 +1,6 @@
+import 'package:aplikasi_kem/ui/widgets/custom_button.dart';
+import 'package:aplikasi_kem/ui/widgets/main_detail_scaffold.dart';
+import 'package:aplikasi_kem/ui/widgets/space.dart';
 import 'package:flutter/material.dart';
 import 'package:aplikasi_kem/utils/ui/color_utils.dart' as colors;
 
@@ -8,13 +11,24 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: colors.backgroundColor,
-      body: Center(
-        child: Text(
-          'Result screen',
-          style: Theme.of(context).textTheme.bodyText1,
-        ),
+    return MainDetailScaffold(
+      appBarTitleText: 'Hasil',
+      appBarTitleTextColor: colors.darkTextColor,
+      illustrationAssetName:
+          'assets/images/kem_increasement/main_illustration.png',
+      tagline:
+          'Sedikit kemajuan setiap hari di dalam dirimu menambah sesuatu hingga hasil yang besar',
+      body: Column(
+        children: [
+          const Space(size: 24.0),
+          CustomButton(
+            text: 'Hasil',
+            onPressed: () {},
+            backgroundColor: colors.blueColor,
+            textColor: colors.whiteColor,
+            type: CustomButtonType.large,
+          ),
+        ],
       ),
     );
   }
