1be132d0b998ee8f9bba161fdf18ffa8897842aa
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-28 19:56:34 +0700

    Added urls data to exercise screens and result

---
+++ lib/data/exercise_urls.dart
@@ -0,0 +1,11 @@
+const exerciseOneUrl =
+    'https://docs.google.com/forms/d/1yQvwXWIhU7DeVbseL_oxEoEDrJdUWOKB5N2b9HQxJ-o/';
+const exerciseTwoUrl =
+    'https://docs.google.com/forms/d/1oB5u3TxxDHSvwKi_x_7tKCJYjEVf57LRokg-L5UQUW4';
+const exerciseThreeUrl =
+    'https://docs.google.com/forms/d/1kw6AReA_-ZD89Vdsue9mjuxIn0-p0OeiiNz6kd6QFLE';
+const exerciseFourUrl =
+    'https://docs.google.com/forms/d/1p25TyEjZvLiy9apNcuRqc6ZHdM1KhDfhQFSGqsSoeZ0';
+const exerciseFiveUrl =
+    'https://docs.google.com/forms/d/1ohtaVfVAEUAZozX4d5oU3_RF8q_2GTFdtstHdgnWUNs';
+const exerciseResultsUrl = 'https://www.mathway.com/BasicMath';
