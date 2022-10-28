a2b0438112680d26022d09b8726cbc3afd7bcf05
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-10-28 21:47:19 +0700

    Updated url_launcher configuration

--- lib/utils/browsable_action.dart
+++ lib/utils/browsable_action.dart
@@ -4,7 +4,11 @@ Future<bool> openUrl(String path) async {
   var parseUrl = Uri.parse(path);
   var result = false;
 
-  if (await canLaunchUrl(parseUrl) && await launchUrl(parseUrl)) {
+  if (await canLaunchUrl(parseUrl) &&
+      await launchUrl(
+        parseUrl,
+        // mode: LaunchMode.externalApplication,
+      )) {
     result = true;
   }
 
