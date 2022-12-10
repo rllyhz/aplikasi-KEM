d239c9db631b2d13360b419c9650c37c2e8a60ff
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-12-11 05:27:13 +0700

    Set up audio files

---
+++ lib/data/audio_utils.dart
@@ -0,0 +1,8 @@
+const kemAudioPath = "kem.m4a";
+const kemIncreasementAudioPath = "kem_increasement.m4a";
+const kemMeasurementAudioPath = "kem_measurement.m4a";
+const readingAudioPath = "reading.m4a";
+
+const audioTitle = "Penjelasan Audio";
+const audioDescription =
+    "Penjelasan Audio ini ada untuk membantu dalam memahami materi. Putar penjelasan audio dibawah ini kemudian dengarkan dengan seksama.";
